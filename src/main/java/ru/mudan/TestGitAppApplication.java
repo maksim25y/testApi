63374c87b2a8840f98b46cc980350fd00026d56c
mudan <myudanov223@gmail.com>
2024-09-18 19:48:00 +0300

    Initial commit

---
+++ src/main/java/ru/mudan/TestGitAppApplication.java
@@ -0,0 +1,13 @@
+package ru.mudan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitAppApplication.class, args);
+    }
+
+}
